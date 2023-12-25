ad8671c5d87cf061a668ad6e0e57a499bc5e37ed
Islem Dj <islemas23@gmail.com>
2023-12-25 04:32:36 +0100

    Exam Page Created

--- js/exam.js
+++ js/exam.js
@@ -19,3 +19,33 @@ numIns.innerHTML = "Matricule : <span>"+ data.numIns + "</span>";
 jour.innerHTML = "Le : <span>" + currentDate.getDate() + " " + monthAbbreviations[currentDate.getMonth()] + "</span>"; 
 heure.innerHTML = "Début a : <span>" + currentDate.getHours() + ":" + currentDate.getMinutes() + "</span>";
 annee.innerHTML = "Année : <span>" + currentDate.getFullYear() + "/" + (currentDate.getFullYear()+1)  + "</span>";
+
+var answers = JSON.parse(localStorage.getItem('answers')) || {
+    qst1 : ''
+};
+
+
+document.getElementById('examForm').addEventListener('submit', function (event) {
+    // Prevent the default form submission
+    event.preventDefault();
+    
+    answers.qst1 = document.querySelector('input[name="radio1"]:checked');
+    
+    
+
+    // // Update formData with the values from the form
+    // formData.nom = document.getElementById('nom').value;
+    // formData.prenom = document.getElementById('prenom').value;
+    // formData.email = document.getElementById('email').value;
+    // formData.numIns = document.getElementById('numIns').value;
+
+    // Log the form data to the console
+    console.log(answers);
+
+    // // Save formData to localStorage
+    localStorage.setItem('answers', JSON.stringify(answers));
+
+    window.location.href = 'examInfo.html';
+
+
+});
